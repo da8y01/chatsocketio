aeabd2c4e75360ac7f57d0b013c3a3c0df1becb8
Balrog <da8y_01@yahoo.com>
2015-05-21 19:51:38 -0500

    adds username and bootstrap styles

--- static/main.js
+++ static/main.js
@@ -6,18 +6,23 @@ $(function() {
 	// declare DOM elements
 	var $app = $('.app');
 	var $messages = $app.find('.messages');
+	var $messagescontainer = $app.find('.messages-container');
 	var $messageinput = $app.find('.messageinput').removeAttr('disabled').focus();
+	var $nameinput = $app.find('.nameinput');
 
 	// receive message
 	socket.on('mensaje', function(message) {
-		$messages.append($('<li>', {'class':'message', text:message.text}));
-		$messages.scrollTop($messages.prop('scrollHeight'));
+		$messages.append($('<li>', {'class':'message', text:message.username+': '+message.text}));
+		$messagescontainer.scrollTop($messagescontainer.prop('scrollHeight'));
 	});
 
 	// send message
 	$messageinput.on('keypress', function(event) {
 		if (event.keyCode == 13) {
-			message = {text: $messageinput.val().trim()}
+			message = {
+				text: $messageinput.val().trim(), 
+				username: $nameinput.val().trim()
+			}
 			console.log('send message: ', message);
 			socket.emit('mensaje', message);
 			$messageinput.val('');
